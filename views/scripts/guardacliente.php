cc764f31e1dfc2a33474c45295bdf010274edad4
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-08-16 13:13:23 -0500

    fdss

--- views/scripts/guardacliente.php
+++ views/scripts/guardacliente.php
@@ -21,7 +21,7 @@
              $insert->verificareg("$nombre_usuario");
               $hash=password_hash($contraseña,PASSWORD_DEFAULT);
               $tiempo = date('Y-m-d');
-              $cadena="INSERT INTO cuenta VALUES('$nombre_usuario','$hash','$nombre','$ap_paterno','$ap_materno','$direccion','$telefono','$correo','','Administrador','$tiempo')";
+              $cadena="INSERT INTO cuenta VALUES('$nombre_usuario','$hash','$nombre','$ap_paterno','$ap_materno','$direccion','$telefono','$correo','','Usuario','$tiempo')";
               $insert->ejecutar($cadena);  
          }     
          else
